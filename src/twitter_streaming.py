9f1acdf3334bd53a4ea2b1c6323d634482269864
Kuang Yang <ky2342@columbia.edu>
2017-03-11 14:18:03 -0500

    Working on es index

--- src/twitter_streaming.py
+++ src/twitter_streaming.py
@@ -16,56 +16,68 @@ class TweetStreamListener(tweepy.StreamListener):
         # Load the json to a string
         cur_data = json.loads(data)
         location = cur_data['user']['location']
-        if location:
-            print location
+        if location and cur_data['lang'] == 'en':
             text = cur_data['text']
+            keyword = getKeyWord(text)
+            api_key = getApiKey()
+            coordinates = getCoordinates(api_key, location)
+            # print cur_data['created_at']
+            if keyword and coordinates:
+                mapping = {
+                    'keyword': keyword,
+                    'author': cur_data['user']['screen_name'],
+                    'text': text,
+                    'timestamp': cur_data['created_at'],
+                    #'location': location,
+                    'coordinates': coordinates
+                }
+                print mapping
 
-            for keyword in FILTERED_KEYWORDS:
-                if keyword in text:
-                    keyword = keyword
-                    break
-                else:
-                    keyword = None
-
-            api_key = "AIzaSyAWjWqvTQl4mrI1dUPJzdJO9O2HyG4Trkc"
-            api_response = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address={0}&key={1}'.format(location, api_key))
-            api_response_dict = api_response.json()
-            if api_response_dict['status'] == "OK":
-                latitude = api_response_dict['results'][0]['geometry']['location']['lat']
-                longitude = api_response_dict['results'][0]['geometry']['location']['lng']
-                coordinates = [latitude, longitude]
-            else:
-                coordinates = None
-
-            print keyword
-            print coordinates
-            mapping = {
-                'keyword': keyword,
-                'author': cur_data['user']['screen_name'],
-                'text': text,
-                'timestamp': datetime.now(),
-                'location': location,
-                'coordinates': coordinates
-            }
-
-            res = self.es.index(index="test-index", doc_type='tweet', id=cur_data['user']['id'], body=mapping)
-            print(res['created'])
+                res = self.es.index(index="twittmap", doc_type='tweet', id=cur_data['user']['id'], body=mapping)
+                print(res['created'])
 
         return True
 
-
     def on_status(self, status):
         print ("Status: " + status.text)
-
 
     def on_error(self, status_code):
         print ("Tweepy Error: " + str(status_code))
         return False
 
 
+def getCoordinates(api_key, location):
+    api_response = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address={0}&key={1}'.format(location, api_key))
+    api_response_dict = api_response.json()
+    if api_response_dict['status'] == "OK":
+        latitude = api_response_dict['results'][0]['geometry']['location']['lat']
+        longitude = api_response_dict['results'][0]['geometry']['location']['lng']
+        coordinates = [latitude, longitude]
+    else:
+        coordinates = None
+    return coordinates
+
+
+def getKeyWord(text):
+    for keyword in FILTERED_KEYWORDS:
+        if keyword in text:
+            keyword = keyword
+            break
+        else:
+            keyword = None
+    return keyword
+
+
+def getApiKey():
+    with open('../config.txt', 'rb') as configfile:
+        api_key = configfile.read().splitlines()[4]
+        configfile.close()
+    return api_key
+
+
 def getCredentials():
     with open('../config.txt', 'rb') as configfile:
-        CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET = configfile.read().splitlines()
+        CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET = configfile.read().splitlines()[0:4]
         configfile.close()
     return CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET
 
@@ -75,7 +87,7 @@ def main():
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
     auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
     api = tweepy.API(auth)
-    es = Elasticsearch()
+    es = Elasticsearch(hosts="search-twittmap-ky-fgzbfoahxmx2okmyighape3sgq.us-east-1.es.amazonaws.com", port=443, use_ssl=True)
     tweetStreamListener = TweetStreamListener(es)
     tweetStream = tweepy.Stream(auth=api.auth, listener=tweetStreamListener)
     tweetStream.filter(track=FILTERED_KEYWORDS, async=True)
