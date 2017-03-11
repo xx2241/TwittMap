238581f8805a1c69a83d57466033c32f2c053b94
Kuang Yang <ky2342@columbia.edu>
2017-03-10 21:30:19 -0500

    Choose to use Tweepy and add config and requirments

--- src/twitter_streaming.py
+++ src/twitter_streaming.py
@@ -1,28 +1,34 @@
 import json
-from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
+import tweepy
+from elasticsearch import Elasticsearch
 
 
-CONSUMER_KEY = 'oic2Xa3hvk019kOX2Qj8aBZvH'
-CONSUMER_SECRET = 'Mm5fUpJ4akUdGrDWRsc2dvZUufccwjBipSRCmRGkXO2Sp8IQhW'
-ACCESS_TOKEN = '734766867538022404-jmLnLYov6P8nMkZW49Aih3e926d1qBx'
-ACCESS_SECRET = 'EEPLwgVUTkW4uTPa13jHI5AuS4lgETmOqNnVIOqFo5VI6'
+class TweetStreamListener(tweepy.StreamListener):
 
-oauth = OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
+    def on_status(self, status):
+        print status.text
 
-twitter_stream = TwitterStream(auth=oauth)
+    def on_error(self, status_code):
+        if status_code == 420:
+            return False
 
-iterator = twitter_stream.statuses.sample()
 
-tweet_count = 1000
-for tweet in iterator:
-    tweet_count -= 1
-    # Twitter Python Tool wraps the data returned by Twitter
-    # as a TwitterDictResponse object.
-    # We convert it back to the JSON format to print/score
-    print json.dumps(tweet)
+def getCredentials():
+    with open('../config.txt', 'rb') as configfile:
+        CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET = configfile.read().splitlines()
+        configfile.close()
+    return CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET
 
-    # The command below will do pretty printing for JSON data, try it out
-    # print json.dumps(tweet, indent=4)
 
-    if tweet_count <= 0:
-        break
+def main():
+    CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET = getCredentials()
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+    api = tweepy.API(auth)
+    tweetStreamListener = TweetStreamListener()
+    tweetStream = tweepy.Stream(auth=api.auth, listener=tweetStreamListener)
+    tweetStream.filter(track=['Trump'], async=True)
+
+
+if __name__ == '__main__':
+    main()
