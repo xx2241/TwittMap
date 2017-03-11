cf4bb50890a517811684c0340f6289405e7a65a8
xx2241 <xx2241@columbia.edu>
2017-03-11 15:53:15 -0500

    googlemap dropdown menu

--- googlemap/googlemap.py
+++ googlemap/googlemap.py
@@ -3,8 +3,12 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
 def hello():
-	
 	return render_template('test.html')
+
+@app.route('/',methods=['GET'])
+def search():
+	keyword = request.args.get('keyword')
+	print(keyword)
 
 
 if __name__ == "__main__":
