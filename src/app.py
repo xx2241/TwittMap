f1014347dca1ee99768808f91d80207e1fe7330b
xx2241 <xx2241@columbia.edu>
2017-03-12 13:01:05 -0400

    successfully plot

--- src/app.py
+++ src/app.py
@@ -4,12 +4,13 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
 def hello():
-	if request == 'POST':
+	if request.method == 'POST':
 		tag = request.form.get('tags')
 		print(tag)
-		return render_template('test.html',locs=tweets_filter('Trump'))
+		print(tweets_filter(tag))
+		return render_template('test2.html',locs=tweets_filter(tag))
 	else:
-		return render_template('test.html',locs=[])
+		return render_template('test2.html',locs=[])
 
 
 if __name__ == "__main__":
