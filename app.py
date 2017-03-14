70f72f896a74fb83be4ee13624ff7d9259b7e595
Kuang Yang <ky2342@columbia.edu>
2017-03-13 20:26:17 -0400

    Seems working

--- app.py
+++ app.py
@@ -4,27 +4,23 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
 def tweetmap():
-	lat = request.args['lat']
-	lon = request.args['lon']
-	print(lat)
-	print(lon)
     if request.method == 'POST':
         tag = request.form.get('tags')
-        print(tag)
-        print(tweets_filter(tag))
-        return render_template('TwittMap.html', locs=tweets_geo([40.8, -73.9]))
+        return render_template('TwittMap.html', locs=[])
     else:
         return render_template('TwittMap.html', locs=[])
-'''
+
+
 @app.route('/local', methods=['GET','POST'])
-def getlocal():
-	#if request.method == 'POST':
-		lat = request.args['lat']
-		lon = request.args['lon']
-		print(lat)
-		print(lon)
-		return render_template('TwittMap.html',locs=[])
-'''
+def tweetmap2():
+    if request.method == 'POST':
+        lat = request.args['lat']
+        lon = request.args['lon']
+        print(lat)
+        print(lon)
+        return render_template('TwittMap.html', locs=[lat, lon])
+    else:
+        return render_template('TwittMap.html', locs=[])
 
 
 if __name__ == "__main__":
