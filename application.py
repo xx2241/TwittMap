705ac2cd3d672fb061d0dd07888a359e301d83fe
Kuang Yang <ky2342@columbia.edu>
2017-03-13 23:49:23 -0400

    0313 Deployed

--- application.py
+++ application.py
@@ -2,16 +2,31 @@ from flask import *
 from twitter_filter import *
 application = Flask(__name__)
 
+
+#@application.route('/', methods=['GET','POST'])
+#def init():
+#    return render_template('TwittMap.html')
+
+
 @application.route('/', methods=['GET','POST'])
 def tweetmap():
-	if request.method == 'POST':
-		tag = request.form.get('tags')
-		print(tag)
-		print(tweets_filter(tag))
-		return render_template('TwittMap.html', locs=tweets_filter(tag))
-	else:
-		return render_template('TwittMap.html', locs=[])
+    if request.method == 'POST':
+        tag = request.form.get('tags')
+        return render_template('TwittMap.html', locs=tweets_filter(tag))
+    else:
+        return render_template('TwittMap.html', locs=[])
+
+
+@application.route('/local', methods=['GET','POST'])
+def tweetgeo():
+    if request.method == 'POST':
+        lat = float(request.args['lat'])
+        lon = float(request.args['lon'])
+        coordinates = [lat, lon]
+        return render_template('TwittMap.html', locs=tweets_geo(coordinates))
+    else:
+        return render_template('TwittMap.html', locs=[])
 
 
 if __name__ == "__main__":
-	application.run(debug=True)
+    application.run(debug=True)
--- twitter_filter.py
+++ twitter_filter.py
@@ -23,7 +23,7 @@ def tweets_geo(coordinates):
                             },
                             "filter" : {
                                 "geo_distance" : {
-                                "distance" : "5km",
+                                "distance" : "50km",
                                 "coordinates" : coordinates
                                 }
                             }
